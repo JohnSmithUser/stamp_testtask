c76c83f9479a45b21949b61c156950d359e9c3eb
Mishchenko.Evgenij <mishchenko.evgenij@innovacia.local>
2025-06-29 17:43:59 +0700

    barrier refactor

--- barrier_state.py
+++ barrier_state.py
@@ -1,10 +1,27 @@
+"""Автомат состояний для определения ТС."""
+
 class BarrierStateMachine:
+    """Класс для автомата состояний."""
+    
     def __init__(self, min_frames_presence=10):
+        """Инициализация автомата состояний.
+
+        Args:
+            min_frames_presence (int): Минимальное количество кадров, в течение которых транспортное средство должно быть обнаружено.
+        """
         self.state = "NO_VEHICLE"
         self.presence_counter = 0
         self.min_frames_presence = min_frames_presence
 
     def update(self, vehicle_detected: bool):
+        """Обновление состояния автомата на основе наличия транспортного средства.
+
+        Args:
+            vehicle_detected (bool): Флаг, указывающий, обнаружено ли транспортное средство.
+
+        Returns:
+            str: Текущее состояние автомата.
+        """
         if vehicle_detected:
             self.presence_counter += 1
         else:
